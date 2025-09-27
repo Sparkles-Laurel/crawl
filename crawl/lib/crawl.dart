fa376a575a5a6f094eb2598ee661147cd30f5dc5
Sparkles-Laurel <sparkles@gayest.systems>
2025-09-27 11:16:30 +0300

    implement link scraper

--- crawl/lib/crawl.dart
+++ crawl/lib/crawl.dart
@@ -5,3 +5,14 @@ library;
 import 'package:crawl/models/link.dart' as link;
 import 'package:crawl/globals.dart' as globals;
 import 'package:crawl/scrape.dart' as scrape;
+
+typedef Page = link.Link;
+
+/// Provides a breadth-first anchor crawler for a web page.
+class Crawler {
+  /// Pairs [link.Link]s against their depths.
+  final Map<Page, int> linkList = <Page, int>{};
+  /// The first [Uri] this [Crawler] will start from.
+  /// This [Uri] will have depth 0
+  late Uri entryPoint = Uri.parse("https://kocaeli.edu.tr");
+}
--- crawl/lib/scrape.dart
+++ crawl/lib/scrape.dart
@@ -1,19 +1,46 @@
+/// Provides a [Scraper] for web pages.
 library;
 
-import 'package:html/parser.dart';
+import 'package:html/parser.dart' as html_parser;
 import 'package:html/dom.dart' as dom;
 import 'package:crawl/models/link.dart' show Link;
-
+/// Scraper for HTML pages to collect links.
 class Scraper {
+  /// The string of the page to load.
   late String pageString;
+  /// The page this scraper will scrape.
   late dom.Document page;
-  Scraper.fromPageString(this.pageString) {
-    page = parse(pageString);
+
+  /// Initializes a [Scraper] from a page string.
+  Scraper.fromDocumentString(this.pageString) {
+    page = html_parser.parse(pageString);
   }
 
-  List<Link> collectLinks() {
+  /// Initializes a [Scraper] from a [dom.Document]
+  Scraper.fromDocument(this.page) {
+    pageString = page.toString();
+  }
+
+  /// Gathers a collection of [Link]s it can find on a web page.
+  /// Note that these links will all have their parents set to [null].
+  /// The parents must be later initialized by a crawler.
+  List<Link>? collectLinks() {
     var result = <Link>[];
 
+    final query = "a";
+    final queryResults = page.body?.querySelectorAll(query) ?? <dom.Element>[];
+    for (var element in queryResults) {
+      if(element.localName == "a") {
+        final href = element.attributes.entries
+          .where((a) => a.key == "href")
+          .map((a) => a.value)
+          .single;
+        final title = element.text;
+        final link = Link(title: title, href: Uri.parse(href));
+
+        result.add(link);
+      }
+    }
     return result;
   } 
 }
