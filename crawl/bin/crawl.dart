763d11d1397cb9869b322a77897ccff31b3f0426
Sparkles-Laurel <sparkles@gayest.systems>
2025-09-28 20:57:35 +0300

    implement crawler

--- crawl/bin/crawl.dart
+++ crawl/bin/crawl.dart
@@ -1,5 +1,22 @@
-import 'package:crawl/crawl.dart' as crawl;
+import 'package:crawl/crawl.dart';
+import 'package:crawl/models/link.dart';
+import 'package:crawl/neo4j.dart' show storeInNeo4j;
 
-void main(List<String> arguments) {
-  print('Crawling utility version 0.1.0');
+void main() async {
+  final entryPoint = Uri.parse('https://kocaeli.edu.tr');
+
+  // create the crawler with a depth limit and page limit
+  final crawler = Crawler.withEntryPoint(
+    entryPoint,
+    maxDepth: 3,
+    maxPages: 1000,
+  );
+
+  print('Starting crawl of $entryPoint...');
+  final Map<Link, num> result = await crawler.crawl();
+  print('Crawl finished. ${result.length} pages visited.');
+
+  // you can now store `result` in Neo4j
+  await storeInNeo4j(result);
 }
+
---
+++ crawl/lib/neo4j.dart
@@ -0,0 +1,43 @@
+/// For interacting with neo4j
+library;
+
+import 'package:crawl/models/link.dart';
+import 'package:dart_neo4j/dart_neo4j.dart';
+
+Future<void> storeInNeo4j(Map<Link, num> map) async {
+  final driver = Neo4jDriver.create("localhost:7878");
+  late Session session;
+  try {
+    await driver.verifyConnectivity();
+    session = driver.session();
+
+    try {
+      // holy Torino i dont know Cypher
+      // holy Torino i barely know anything about graph databases
+      // holy fucking Chronos i dont know anything about neo4j either
+      // guess it will be a fun experience to learn it here.,
+      final query = r'''
+    MERGE (l:Link {href: $href})
+    SET l.title = $title, l.depth = $depth
+    WITH l
+    OPTIONAL MATCH (p:Link {href: $parentHref})
+    WHERE $parentHref IS NOT NULL
+    MERGE (l)-[:HAS_PARENT]->(p)
+  ''';
+
+      for (final entry in map.entries) {
+        final link = entry.key;
+        final depth = entry.value;
+
+        await session.run(query, {
+          'href': link.href.toString(),
+          'title': link.title,
+          'depth': depth,
+          'parentHref': link.parent?.href.toString(),
+        });
+      }
+    } finally {}
+  } finally {
+    await session.close();
+  }
+}
